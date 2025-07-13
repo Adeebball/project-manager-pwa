12f3390f0578074789420cc5a79550e6fd7dd5bd
Adeebball <98996018+Adeebball@users.noreply.github.com>
2025-07-13 04:35:31 +0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-const CACHE_NAME = 'app-cache-v1';
-const urlsToCache = ['/', 'index.html', 'style.css', 'app.js', 'manifest.json'];
-
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))).then(() => self.skipWaiting()));
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
